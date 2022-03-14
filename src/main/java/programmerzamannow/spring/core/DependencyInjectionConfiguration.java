ebf78b41f04ef00072956e68bd584e98a5bc97e7
Albert Kristaen <albert.kristaen@aia.com>
2022-03-14 15:00:48 +0800

    11. dependency injection

---
+++ src/main/java/programmerzamannow/spring/core/DependencyInjectionConfiguration.java
@@ -0,0 +1,26 @@
+package programmerzamannow.spring.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import programmerzamannow.spring.core.data.Bar;
+import programmerzamannow.spring.core.data.Foo;
+import programmerzamannow.spring.core.data.FooBar;
+
+@Configuration
+public class DependencyInjectionConfiguration {
+
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        return new Bar();
+    }
+
+    @Bean
+    public FooBar fooBar(Foo foo, Bar bar) {
+        return new FooBar(foo, bar);
+    }
+}
---
+++ src/test/java/programmerzamannow/spring/core/DependecyInjectionTest.java
@@ -0,0 +1,47 @@
+package programmerzamannow.spring.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import programmerzamannow.spring.core.data.Bar;
+import programmerzamannow.spring.core.data.Foo;
+import programmerzamannow.spring.core.data.FooBar;
+
+public class DependecyInjectionTest {
+
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(DependencyInjectionConfiguration.class);
+    }
+
+    @Test
+    void testDI() {
+
+        Foo foo = applicationContext.getBean(Foo.class);
+        Bar bar = applicationContext.getBean(Bar.class);
+        FooBar foobar = applicationContext.getBean(FooBar.class);
+
+        Assertions.assertSame(foo, foobar.getFoo());
+        Assertions.assertSame(bar, foobar.getBar());
+
+    }
+
+    @Test
+    void testNoDI() {
+
+        var foo = new Foo();
+
+        var bar = new Bar();
+
+        var foobar = new FooBar(foo, bar);
+
+        Assertions.assertSame(foo, foobar.getFoo());
+        Assertions.assertSame(bar, foobar.getBar());
+
+    }
+
+}
