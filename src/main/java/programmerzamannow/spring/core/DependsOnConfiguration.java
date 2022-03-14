ded7faa9a8fb8f593eaa1444818f1c1b6f28d1b4
Albert Kristaen <albert.kristaen@aia.com>
2022-03-14 16:39:39 +0800

    14. depends on

---
+++ src/main/java/programmerzamannow/spring/core/DependsOnConfiguration.java
@@ -0,0 +1,29 @@
+package programmerzamannow.spring.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import programmerzamannow.spring.core.data.Bar;
+import programmerzamannow.spring.core.data.Foo;
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn({
+        "bar"
+    })
+    public Foo foo() {
+        log.info("Create new Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        log.info("Create new Bar");
+        return new Bar();
+    }
+
+}
