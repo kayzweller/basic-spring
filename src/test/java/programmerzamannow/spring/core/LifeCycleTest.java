566dded405147c27db408990abf84642e78d5574
Albert Kristaen <kayzweller@gmail.com>
2022-07-16 11:58:41 +0800

    18. life cycle

---
+++ src/test/java/programmerzamannow/spring/core/LifeCycleTest.java
@@ -0,0 +1,29 @@
+package programmerzamannow.spring.core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import programmerzamannow.spring.core.data.Connection;
+
+public class LifeCycleTest {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @AfterEach
+    void tearDown() {
+        // applicationContext.close();
+    }
+
+    @Test
+    void testConnection() {
+        applicationContext.getBean(Connection.class);
+    }
+}
